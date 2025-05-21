9e0468a04c3998c0069f454af9af3d456cd98715
martinkargl <martin.kargl@tuwien.ac.at>
2025-05-21 22:43:43 +0200

    static export

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enable static export for GitHub Pages
 };
 
 export default nextConfig;
